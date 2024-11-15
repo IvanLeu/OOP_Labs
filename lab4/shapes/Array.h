528f941e5015a3685e10eecf46563271ffec24f2
ivanleu14@gmail.com <ivanleu14@gmail.com>
2024-11-15 23:34:05 +0300

    templatelized

--- lab4/shapes/Array.h
+++ lab4/shapes/Array.h
@@ -2,30 +2,144 @@
 #include "Shapes.h"
 #include <initializer_list>
 
+template<Vert T>
 class Array {
 public:
 	Array() = default;
-	Array(size_t capacity) : m_capacity(capacity) {}
-	Array(const std::initializer_list<Shape*>& shapes);
-	Array(const Array&);
-	Array& operator=(const Array&);
-	Array(Array&&) noexcept;
-	Array& operator=(Array&&) noexcept;
-	~Array();
-	void push_back(const Shape* shape);
-	void pop_back();
-	void erase(size_t index);
-	const Shape* operator[](int index) const;
-	Shape* operator[](int index);
-	bool empty() const;
-	size_t size() const;
-	size_t capacity() const;
-	void reserve(size_t new_capacity);
-	Shape** data() const;
-	Shape** begin() const;
-	Shape** end() const;
+	Array(const std::initializer_list<Shape*>& shapes)
+	{
+		m_capacity = shapes.size();
+		m_size = shapes.size();
+		m_shapes = new Shape * [m_capacity];
+
+		std::copy_n(shapes.begin(), m_size, begin());
+	}
+	Array(const Array& other)
+	{
+		m_size = other.m_size;
+		m_capacity = other.m_capacity;
+		delete[] m_shapes;
+
+		m_shapes = new Shape * [m_capacity];
+		std::copy(other.begin(), other.end(), begin());
+	}
+	Array& operator=(const Array& other)
+	{
+		if (this != &other) {
+			m_size = other.m_size;
+			m_capacity = other.m_capacity;
+			delete[] m_shapes;
+
+			m_shapes = new Shape * [m_capacity];
+			std::copy(other.begin(), other.end(), begin());
+		}
+
+		return *this;
+	}
+	Array(Array&& other) noexcept
+	{
+		m_size = other.m_size;
+		m_capacity = other.m_capacity;
+		delete[] m_shapes;
+
+		m_shapes = other.m_shapes;
+		other.m_shapes = nullptr;
+	}
+	Array& operator=(Array&& other) noexcept
+	{
+		if (this != &other) {
+			m_size = other.m_size;
+			m_capacity = other.m_capacity;
+			delete[] m_shapes;
+
+			m_shapes = other.m_shapes;
+			other.m_shapes = nullptr;
+		}
+		return *this;
+	}
+	~Array()
+	{
+		delete[] m_shapes;
+	}
+	void push_back(const Shape* shape)
+	{
+		if (m_size < m_capacity) {
+			memcpy(m_shapes[m_size++], shape, sizeof(Shape));
+		}
+		else {
+			m_capacity *= 2;
+			auto new_buf = new Shape * [m_capacity];
+			std::copy(begin(), end(), new_buf);
+			delete[] m_shapes;
+			m_shapes = new_buf;
+
+			memcpy(m_shapes[m_size++], shape, sizeof(Shape));
+		}
+	}
+	void pop_back()
+	{
+		m_shapes[m_size--]->~Shape();
+	}
+	void erase(size_t index)
+	{
+		if (index >= 0 && index <= m_size) {
+			std::swap(m_shapes[m_size], m_shapes[index]);
+			pop_back();
+		}
+	}
+	const Shape<T>* operator[](int index) const
+	{
+		if (index >= 0 && index <= m_size) {
+			return this->m_shapes[index];
+		}
+		return nullptr;
+	}
+	Shape<T>* operator[](int index)
+	{
+		if (index >= 0 && index <= m_size) {
+			return this->m_shapes[index];
+		}
+		return nullptr;
+	}
+	bool empty() const
+	{
+		return m_size == 0;
+	}
+	size_t size() const
+	{
+		return m_size;
+	}
+	size_t capacity() const
+	{
+		return m_capacity;
+	}
+	void reserve(size_t new_capacity)
+	{
+		m_capacity = new_capacity;
+		if (!m_shapes) {
+			m_shapes = new Shape * [m_capacity];
+		}
+		else {
+			auto new_buf = new Shape * [m_capacity];
+			std::copy(begin(), end(), new_buf);
+			delete[] m_shapes;
+			m_shapes = new_buf;
+		}
+	}
+	Shape<T>** data() const
+	{
+		return m_shapes;
+	}
+	Shape<T>** begin() const
+	{
+		return m_shapes;
+	}
+	Shape<T>** end() const
+	{
+		return m_shapes + m_size;
+	}
 private:
 	size_t m_size = 0;
 	size_t m_capacity = 0;
-	Shape** m_shapes = nullptr;
+	Shape<T>** m_shapes = nullptr;
 };
--- lab4/shapes/Shapes.h
+++ lab4/shapes/Shapes.h
@@ -4,10 +4,17 @@
 #include <string>
 #include <assert.h>
 #include <iostream>
+#include <type_traits>
 
+template<class T>
+concept Vert = requires {
+	std::is_scalar_v<T>;
+};
+
+template<Vert T>
 struct Vertex {
 	Vertex() = default;
-	Vertex(float x_in, float y_in) 
+	Vertex(T x_in, T y_in)
 		: 
 		x(x_in), y(y_in) {}
 	Vertex operator+(const Vertex& v_in) const {
@@ -43,89 +50,208 @@ struct Vertex {
 		return is;
 	}
 public:
-	float x = 0.0f;
-	float y = 0.0f;
+	T x = 0.0f;
+	T y = 0.0f;
 };
 
+template<Vert T>
 class Shape {
 public:
-	virtual Vertex GetCenter() const = 0;
+	virtual Vertex<T> GetCenter() const = 0;
 	virtual double GetArea() const = 0;
 	virtual explicit operator double() const = 0;
 };
 
-class Polygon : public Shape {
+template<Vert T>
+class Polygon : public Shape<T> {
 protected:
-	Polygon(const std::string& name_in, const std::vector<Vertex>& vertices_in)
+	Polygon(const std::string& name_in, const std::vector<Vertex<T>>& vertices_in)
 		:
 		shapeName(name_in),
 		vertices(vertices_in)
 	{}
 public:
-	friend std::ostream& operator<<(std::ostream& os, const Polygon& shape);
+	friend std::ostream& operator<<(std::ostream& os, const Polygon& shape)
+	{
+		os << shape.GetName() << ": " << " { ";
+		for (auto& v : shape.vertices) {
+			os << v;
+		}
+		os << "}";
+
+		return os;
+	}
 	virtual inline std::string GetName() const { return shapeName; }
 	virtual inline std::vector<Vertex> GetVertices() const { return vertices; }
 	inline void SetVertices(const std::vector<Vertex>& verts) { vertices = verts; }
 	virtual ~Polygon() = default;
-	bool operator==(const Polygon& rhs) const;
+	bool operator==(const Polygon& rhs) const {
+		if (GetVertices().size() != rhs.GetVertices().size())
+		{
+			return false;
+		}
+
+		auto size = GetVertices().size();
+		for (int i = 0, j = 0; j < size; ++j)
+		{
+			if (i == size)
+			{
+				return true;
+			}
+			else if (vertices[i] == rhs.vertices[j])
+			{
+				++i;
+				j = -1;
+			}
+		}
+		return false;
+	}
 protected:
 	std::string shapeName;
-	std::vector<Vertex> vertices;
+	std::vector<Vertex<T>> vertices;
 };
 
-class Triangle : public Polygon {
+template<Vert T>
+class Triangle : public Polygon<T> {
 public:
-	Triangle(const std::vector<Vertex>& vertices_in)
+	Triangle(const std::vector<Vertex<T>>& vertices_in)
 		:
 		Polygon("Triangle", vertices_in)
 	{
 		assert(vertices_in.size() == 3);
 	}
-	virtual Vertex GetCenter() const override;
-	virtual double GetArea() const override;
-	virtual explicit operator double() const override;
-	bool operator==(const Triangle& rhs) const;
-	friend std::istream& operator>>(std::istream& is, Triangle& shape);
+	virtual Vertex<T> GetCenter() const override {
+		auto tri = GetVertices();
+
+		float centerX = (tri[0].x + tri[1].x + tri[2].x) / 3.0f;
+		float centerY = (tri[0].y + tri[1].y + tri[2].y) / 3.0f;
+
+		return Vertex<T>{ centerX, centerY };
+	}
+	virtual double GetArea() const override {
+		auto verts = GetVertices();
+
+		double area = 0.5 * abs((verts[0].x - verts[2].x) * (verts[1].y - verts[0].y)
+			- (verts[0].x - verts[1].x) * (verts[2].y - verts[0].y));
+
+		return area;
+	}
+	virtual explicit operator double() const override {
+		return GetArea();
+	}
+	bool operator==(const Triangle& rhs) const {
+		for (int i = 0, j = 0; j < 3; ++j)
+		{
+			if (i == 3)
+			{
+				return true;
+			}
+			else if (vertices[i] == rhs.vertices[j])
+			{
+				++i;
+				j = -1;
+			}
+		}
+		return false;
+	}
+	friend std::istream& operator>>(std::istream& is, Triangle& shape) {
+		std::vector<Vertex<T>> verts;
+
+		is >> verts[0];
+		is >> verts[1];
+		is >> verts[2];
+
+		tri.SetVertices(verts);
+
+		return is;
+	}
 };
 
-class Square : public Polygon {
+template<Vert T>
+class Square : public Polygon<T> {
 public:
-	Square(const Vertex& pos, float width)
+	Square(const Vertex<T>& pos, T width)
 		:
 		pos(pos),
 		width(width),
 		Polygon("Square", FromWidth(pos, width))
 	{}
-	virtual Vertex GetCenter() const override;
-	virtual explicit operator double() const override;
-	virtual double GetArea() const override;
-	friend std::istream& operator>>(std::istream& is, Square& shape);
-	bool operator==(const Square& rhs) const;
+	virtual Vertex<T> GetCenter() const override {
+		return Vertex<T>{ pos.x + width * (T)0.5f, pos.y + width * (T)0.5f };
+	}
+	virtual explicit operator double() const override {
+		return GetArea();
+	}
+	virtual double GetArea() const override {
+		return width * width;
+	}
+	friend std::istream& operator>>(std::istream& is, Square& shape) {
+		is >> sqr.pos;
+		is >> sqr.width;
+
+		return is;
+	}
+	bool operator==(const Square& rhs) const {
+		return width == rhs.width && pos == rhs.pos;
+	}
 private:
-	static std::vector<Vertex> FromWidth(const Vertex& v, float width);
+	static std::vector<Vertex<T>> FromWidth(const Vertex<T>& v, float width) {
+		std::vector<Vertex> verts;
+
+		verts.push_back(v);
+		verts.push_back({ v.x + width, v.y });
+		verts.push_back({ v.x, v.y + width });
+		verts.push_back({ v.x + width, v.y + width });
+
+		return verts;
+	}
 private:
-	Vertex pos;
-	float width;
+	Vertex<T> pos;
+	T width;
 };
 
-class Rectangle : public Polygon {
+template<Vert T>
+class Rectangle : public Polygon<T> {
 public:
-	Rectangle( const Vertex& pos, float width, float height )
+	Rectangle( const Vertex<T>& pos, T width, T height )
 		:
 		pos(pos),
 		width(width),
 		height(height),
 		Polygon("Rectangle", FromWidthAndHeight(pos, width, height))
 	{}
-	virtual Vertex GetCenter() const override;
-	virtual explicit operator double() const override;
-	virtual double GetArea() const override;
-	friend std::istream& operator>>(std::istream& is, Rectangle& shape);
-	bool operator==(const Rectangle& rhs) const;
+	virtual Vertex<T> GetCenter() const override {
+		return Vertex<T>{ pos.x + width * (T)0.5f, pos.y + height * (T)0.5f };
+	}
+	virtual explicit operator double() const override {
+		return GetArea();
+	}
+	virtual double GetArea() const override {
+		return width * height;
+	}
+	friend std::istream& operator>>(std::istream& is, Rectangle& shape) {
+		is >> shape.pos;
+		is >> shape.width;
+		is >> shape.height;
+
+		return is;
+	}
+	bool operator==(const Rectangle& rhs) const {
+		return width == rhs.width && height == rhs.height && pos == rhs.pos;
+	}
 private:
-	static std::vector<Vertex> FromWidthAndHeight(const Vertex& v, float width, float height);
+	static std::vector<Vertex<T>> FromWidthAndHeight(const Vertex<T>& v, T width, T height) {
+		std::vector<Vertex<T>> verts;
+
+		verts.push_back(v);
+		verts.push_back({ v.x + width, v.y });
+		verts.push_back({ v.x, v.y + height });
+		verts.push_back({ v.x + width, v.y + height });
+
+		return verts;
+	}
 private:
-	Vertex pos;
-	float width;
-	float height;
+	Vertex<T> pos;
+	T width;
+	T height;
 };
